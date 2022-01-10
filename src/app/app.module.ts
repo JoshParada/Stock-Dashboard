83dbdee605333260fa067c485d0d5df787e836f9
JoshParada <54327710+JoshParada@users.noreply.github.com>
2022-01-10 11:35:06 -0600

    created project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SearchpageComponent } from './searchpage/searchpage.component';
+import { StockpageComponent } from './stockpage/stockpage.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    SidebarComponent,
+    HomepageComponent,
+    SearchpageComponent,
+    StockpageComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/stockpage/stockpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'stockpage',
+  templateUrl: './stockpage.component.html',
+  styleUrls: ['./stockpage.component.scss']
+})
+export class StockpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
