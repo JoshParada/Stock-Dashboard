c0173c0378b676b667c815227136496a82041baa
JoshParada <54327710+JoshParada@users.noreply.github.com>
2022-01-18 09:58:22 -0600

    deploy2

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/stock-dashboard'));
+app.use(express.static(__dirname + '/dist/StockDashboard'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/stock-dashboard/index.html'));});
+'/dist/StockDashboard/index.html'));});
 app.listen(process.env.PORT || 8080);
